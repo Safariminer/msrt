54e3ce76a6d9de2f491f69231c33fbc5877422d9
Thomas (a.k.a. CatArtik) <74744603+Safariminer@users.noreply.github.com>
2024-06-03 21:30:37 -0400

    MSRT_CUSTOM_FUNCTIONS_NEEDED
    Adds a clause so that if you define MSRT_CUSTOM_FUNCTIONS_NEEDED the functionMap map is exposed.

--- msrt/msrt_main.h
+++ msrt/msrt_main.h
@@ -10,6 +10,13 @@ Written by Safariminer
 
 #include <iostream>
 #include "msrt_tokens.h"
+#include <map>
+#include "msrt_types.h"
+
+#ifdef MSRT_CUSTOM_FUNCTIONS_NEEDED
+extern std::map<std::string, MSRT::Function*> functionMap;
+#endif
+
 namespace MSRT {
 
 	void InitMSRT(std::string file, bool dataread = false, bool verboseparsing = false); // All you truly need.
